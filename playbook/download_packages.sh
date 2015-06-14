229c26a1f0a18417cb627985fbc226d7d58d0031
David Greco <greco@acm.org>
2015-06-14 15:29:02 +0200

    upgraded spark to the latest version

--- playbook/download_packages.sh
+++ playbook/download_packages.sh
@@ -10,4 +10,4 @@ popd  > /dev/null
 (rm -rf $SCRIPT_PATH/packages; mkdir $SCRIPT_PATH/packages)
 (cd $SCRIPT_PATH/packages; wget http://archive.cloudera.com/cdh5/cdh/5/zookeeper-3.4.5-cdh5.4.2.tar.gz)
 (cd $SCRIPT_PATH/packages; wget http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.6.0-cdh5.4.2.tar.gz)
-(cd $SCRIPT_PATH/packages; wget http://d3kbcqa49mib13.cloudfront.net/spark-1.3.1-bin-hadoop2.6.tgz)
+(cd $SCRIPT_PATH/packages; wget http://www.us.apache.org/dist/spark/spark-1.4.0/spark-1.4.0-bin-without-hadoop.tgz)
