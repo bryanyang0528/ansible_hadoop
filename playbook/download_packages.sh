8f7ace42573d1b26376a308443b76aeb9274f414
David Greco <greco@acm.org>
2015-08-30 20:07:44 +0200

    updated to the latest version of cdh

--- playbook/download_packages.sh
+++ playbook/download_packages.sh
@@ -8,6 +8,6 @@ cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 (rm -rf $SCRIPT_PATH/packages; mkdir $SCRIPT_PATH/packages)
-(cd $SCRIPT_PATH/packages; wget http://archive.cloudera.com/cdh5/cdh/5/zookeeper-3.4.5-cdh5.4.3.tar.gz)
-(cd $SCRIPT_PATH/packages; wget http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.6.0-cdh5.4.3.tar.gz)
-(cd $SCRIPT_PATH/packages; wget http://www.us.apache.org/dist/spark/spark-1.4.0/spark-1.4.0-bin-without-hadoop.tgz)
+(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/zookeeper-3.4.5-cdh5.4.5.tar.gz)
+(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.6.0-cdh5.4.5.tar.gz)
+(cd $SCRIPT_PATH/packages; wget -c http://www.us.apache.org/dist/spark/spark-1.4.1/spark-1.4.1-bin-without-hadoop.tgz)
