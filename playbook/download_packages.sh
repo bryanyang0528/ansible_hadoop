db835b12e68f3346f6ae5c17ec8ad1340e4eaeda
David Greco <greco@acm.org>
2015-10-26 09:07:53 +0100

    updated to cdh5.4.7

--- playbook/download_packages.sh
+++ playbook/download_packages.sh
@@ -8,7 +8,7 @@ cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 #(rm -rf $SCRIPT_PATH/packages; mkdir $SCRIPT_PATH/packages)
-(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/zookeeper-3.4.5-cdh5.4.5.tar.gz)
-(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.6.0-cdh5.4.5.tar.gz)
-(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/hive-1.1.0-cdh5.4.5.tar.gz)
-(cd $SCRIPT_PATH/packages; wget -c http://www.us.apache.org/dist/spark/spark-1.5.0/spark-1.5.0-bin-without-hadoop.tgz)
+(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/zookeeper-3.4.5-cdh5.4.7.tar.gz)
+(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.6.0-cdh5.4.7.tar.gz)
+(cd $SCRIPT_PATH/packages; wget -c http://archive.cloudera.com/cdh5/cdh/5/hive-1.1.0-cdh5.4.7.tar.gz)
+(cd $SCRIPT_PATH/packages; wget -c http://www.us.apache.org/dist/spark/spark-1.5.1/spark-1.5.1-bin-without-hadoop.tgz)
