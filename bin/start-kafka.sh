06b98117c8156c3cc9c5a3fd4b4ef530ccfb91a4
David Greco <greco@acm.org>
2015-11-07 16:54:02 +0100

    added support for kafka

---
+++ bin/start-kafka.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+ansible-playbook --sudo -i $SCRIPT_PATH/../playbook/hosts $SCRIPT_PATH/../playbook/tasks/start_kafka.yml
